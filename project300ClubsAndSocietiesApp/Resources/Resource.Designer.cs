3e698e0032c3bed239fbc827365918705dd64ef0
s00165311 <s00165311@mail.itsligo.ie>
2017-11-27 04:58:18 +0000

    Small Fixes

--- project300ClubsAndSocietiesApp/Resources/Resource.Designer.cs
+++ project300ClubsAndSocietiesApp/Resources/Resource.Designer.cs
@@ -48,7 +48,10 @@ namespace project300ClubsAndSocietiesApp
 			public const int buttonStyle = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int progressBarStyle = 2130837505;
+			public const int editTextStyle = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int progressBarStyle = 2130837506;
 			
 			static Drawable()
 			{
